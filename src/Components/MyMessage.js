1258bc43c6e8dca22ba50f16ad78ede62a5aad73
keneishac <keneishacorbett@gmail.com>
2021-02-22 14:57:40 -0800

    adds funtion to check if message contains an image

--- src/Components/MyMessage.js
+++ src/Components/MyMessage.js
@@ -1,7 +1,18 @@
-function MyMessage() {
+function MyMessage({ message }) {
+    //checks to see if message contains an attachment
+    if(message?.attachment?.length > 0) {
+         return
+          <img 
+            src={message.attachments[0].file}
+            alt='message-attachment'
+            className='message-image'
+            style={{float: 'right'}}
+          />
+    }
+
     return (
-        <div>
-            My Message
+        <div className='message' style={{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}} >
+            {message.text}
         </div>
     )
 }
