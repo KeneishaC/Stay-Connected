0fadfd3467d3d98fcf06e3ccce332ac6a701eed8
keneishac <keneishacorbett@gmail.com>
2021-02-22 13:34:01 -0800

    setus comonents fro chat feed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from 'react-chat-engine'
 import './App.css'
 
+import ChatFeed from './Components/ChatFeed'
 
 function App() {
   return (
      <ChatEngine 
         height='100vh'
-        projectID=''
-        userName=''
-        userSecret=''
+        projectID='a1967ba2-df19-43a8-9eb7-dfb57a51dd7d'
+        userName='swekeneisha'
+        userSecret='Sjc*1993'
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
      />
   );
 }
---
+++ src/Components/ChatFeed.js
@@ -0,0 +1,15 @@
+import MessageForm from './MessageForm'
+import TheirMessage from './TheirMessage'
+import MyMessage from './MyMessage'
+
+function ChatFeed() {
+    return (
+        <div>
+            ChatFeed
+        </div>
+    )
+}
+
+export default ChatFeed
+
+
