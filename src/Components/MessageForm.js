a8f0e50e8fd0ee1363562a50291af6fcac5aa536
keneishac <keneishacorbett@gmail.com>
2021-02-23 16:20:02 -0800

    adds text input form

--- src/Components/MessageForm.js
+++ src/Components/MessageForm.js
@@ -1,8 +1,36 @@
-function MessageForm() {
+import { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
+
+
+function MessageForm(props) {
+    const [value, setValue] = useState('')
+    const { chatId, creds } = props
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const text = value.trim()
+
+        if(text.length  > 0 )sendMessage(creds, chatId, { text })
+
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+
+        isTyping(props, chatId)
+
+    }
+
     return (
-        <div>
-            Message Form
-        </div>
+        <form className='messsage-form' onSubmit={handleSubmit}>
+            <input 
+                className='message-input'
+                placeholder='Send a message'
+                value={ value }
+                onChange={ handleChange }
+                onSubmit={ handleSubmit }
+            />
+        </form>
     )
 }
 
