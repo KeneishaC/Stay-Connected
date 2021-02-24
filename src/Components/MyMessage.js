a8caff96300dc311de3e50da77342631bd470f13
keneishac <keneishacorbett@gmail.com>
2021-02-24 11:55:41 -0800

    code refactor

--- src/Components/MyMessage.js
+++ src/Components/MyMessage.js
@@ -1,21 +1,22 @@
 function MyMessage({ message }) {
     //checks to see if message contains an attachment
     if (message.attachments && message.attachments.length > 0) {
-        return (
-          <img
-            src={message.attachments[0].file}
-            alt="message-attachment"
-            className="message-image"
-            style={{ float: 'right' }}
-          />
-        );
-      }
-    
       return (
-        <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
-          {message.text}
-        </div>
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: 'right' }}
+        />
       );
+    }
+  
+    return (
+      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+        {message.text}
+      </div>
+    );
+  };
     
 }
 
