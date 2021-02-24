08358d0f4292cd40024c89249a31064457a7868b
keneishac <keneishacorbett@gmail.com>
2021-02-23 17:06:03 -0800

    fixes bugs

--- src/Components/MyMessage.js
+++ src/Components/MyMessage.js
@@ -1,21 +1,22 @@
 function MyMessage({ message }) {
     //checks to see if message contains an attachment
-    if(message?.attachment?.length > 0) {
-         return (
-          <img 
+    if (message.attachments && message.attachments.length > 0) {
+        return (
+          <img
             src={message.attachments[0].file}
-            alt='message-attachment'
-            className='message-image'
-            style={{float: 'right'}}
+            alt="message-attachment"
+            className="message-image"
+            style={{ float: 'right' }}
           />
-         )
-    }
-
-    return (
-        <div className='message' style={{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}} >
-            {message.text}
+        );
+      }
+    
+      return (
+        <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+          {message.text}
         </div>
-    )
+      );
+    
 }
 
 export default MyMessage
