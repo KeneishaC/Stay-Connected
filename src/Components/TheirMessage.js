d0805779fdd8046b169632876c09320b59bf4d29
keneishac <keneishacorbett@gmail.com>
2021-02-23 16:09:54 -0800

    adds function for showing what other user has sent

--- src/Components/TheirMessage.js
+++ src/Components/TheirMessage.js
@@ -1,7 +1,35 @@
-function TheirMessage() {
+function TheirMessage({ lastMesaage, message }) {
+    //first message user sees if message wan't sent by user
+    const isFirstMessageByUser = !lastMesaage || lastMesaage.sender.username !== message.sender.username
+
+
+
     return (
-        <div>
-            Their Message
+        <div className='mesage-row'>
+            {
+                isFirstMessageByUser && (
+                    <div 
+                    className='message-avatar'
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                    
+                    />
+                )
+            }
+            {/* if message usser see contains an image */}
+            {message?.attachment?.length > 0
+            ? (
+                <img 
+                    src={message.attachments[0].file}
+                    alt='message-attachment'
+                    className='message-image'
+                    style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                />
+
+            ) : (
+                <div className='message' style={{float: 'left', backgroundColor: '#CABCDC'}} >
+                 {message.text}
+                </div>
+            )}
         </div>
     )
 }
